da2dc00b7633bb02917ae66ad2c65e8aed5b09b4
Takahiro Fukushima <fukkuaaa@gmail.com>
2023-12-17 20:54:13 +0900

    faiss operatorの修正

--- src/lib/faiss_operator.py
+++ src/lib/faiss_operator.py
@@ -1,11 +1,17 @@
 import faiss
+import torch
 
 
 class FaissOperator:
     @staticmethod
-    def create_faiss_index(vectors):
+    def create_faiss_index(vectors, with_cuda=False):
         dimension = vectors.shape[1]
         index = faiss.IndexFlatL2(dimension)
+
+        if with_cuda:
+            gpu_resource = faiss.StandardGpuResources()
+            index = faiss.index_cpu_to_gpu(gpu_resource, 0, index)
+
         index.add(vectors)
         return index
 
@@ -16,8 +22,16 @@ class FaissOperator:
 
     @staticmethod
     def save_faiss_index(index, file_path):
+        if faiss.get_num_gpus() > 0:
+            index = faiss.index_gpu_to_cpu(index)
         faiss.write_index(index, file_path)
 
     @staticmethod
-    def load_faiss_index(file_path):
-        return faiss.read_index(file_path)
+    def load_faiss_index(file_path, with_cuda=False):
+        index = faiss.read_index(file_path)
+        if with_cuda:
+            if not torch.cuda.is_available():
+                raise RuntimeError("CUDA is not available.")
+            gpu_resource = faiss.StandardGpuResources()
+            index = faiss.index_cpu_to_gpu(gpu_resource, 0, index)
+        return index
