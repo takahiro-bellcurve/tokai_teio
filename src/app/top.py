9a7a728b00f80c992420302deb9b6b019f146f7f
Takahiro Fukushima <fukkuaaa@gmail.com>
2023-12-20 02:20:20 +0900

    add search similar clothing page

---
+++ src/app/top.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Tokai Teio",
+)
+
+st.write("# Tokai Teio DEMO APP")
+
+st.markdown("""
+### 研究用レポジトリ: [GitHub](https://github.com/takahiro-bellcurve/tokai_teio)
+衣服の画像検索システムのデモアプリです。
+https://tokaiteio.sodashi.cc/search_similar_clothing のページで画像をアップロードし、似た衣服を検索することができます。
+""")
