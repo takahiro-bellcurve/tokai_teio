fa7efe3fe38e1334a9c25dfb61a0c57598764e57
Takahiro Fukushima <fukkuaaa@gmail.com>
2024-01-15 16:06:32 +0900

    umap visualizationを修正

--- src/app/pages/umap_visualization_with_img.py
+++ src/app/pages/umap_visualization_with_img.py
@@ -1,17 +1,19 @@
 import os
 import re
 import io
-import pickle
-import umap
 import logging
-import matplotlib.pyplot as plt
+import pickle
+
+import umap
 import numpy as np
-
-
 import streamlit as st
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import (AnnotationBbox, DrawingArea, OffsetImage,
+                                  TextArea)
 
 from src.lib.setup_logging import setup_logging
 from src.lib.system_operator import get_gpu_memory_usage
+from src.lib.image_preprocessor import ImagePreprocessor
 
 setup_logging()
 logger = logging.getLogger(__name__)
@@ -37,6 +39,14 @@ st.text(f"latent_dim: {latent_dim}")
 st.markdown(f"GPUメモリ使用率 {get_gpu_memory_usage()}%")
 
 
+def get_image(file_name, ax, xy, zoom):
+    img = ImagePreprocessor.open_image(f"data/train_data/{file_name}")
+    im = OffsetImage(img, zoom=zoom)
+    ab = AnnotationBbox(im, xy, xybox=(0., 0.),
+                        frameon=False, boxcoords="data", pad=0.0)
+    ax.add_artist(ab)
+
+
 def umap_visualization():
     # pickleファイルのパス
     pickle_file = f'trained_models/vectors/{model_name}.pkl'
@@ -56,7 +66,8 @@ def umap_visualization():
     latent_vectors = np.array(latent_vectors).reshape(len(latent_vectors), -1)
     embedding = reducer.fit_transform(latent_vectors)
 
-    fig = plt.figure()
+    fig, ax = plt.subplots(figsize=(12, 10))
+    ax.set_aspect('equal')
 
     # ユニークなカテゴリのリストを作成
     unique_labels = np.unique(labels)
@@ -71,16 +82,25 @@ def umap_visualization():
         indices = [j for j, l in enumerate(labels) if l == label]
         plt.scatter(embedding[indices, 0],
                     embedding[indices, 1], color=colors[i], label=label)
+    zoom_factor = 0.1  # ズームの調整
+    for data, (x, y) in zip(latent_vectors_with_metadata, embedding):
+        url = data[0]['file_name']
+        get_image(url, ax, (x, y), zoom_factor)
 
     plt.gca().set_aspect('equal', 'datalim')
     plt.title('UMAP projection of the dataset', fontsize=24)
     plt.legend(markerscale=2)
-    st.session_state["fig"] = fig
+    buf = io.BytesIO()
+    fig.savefig(
+        f"./generated_images/umap/{model_name}_nn{n_neighbors}_md{min_dist}_s{spread}.png")
+    fig.savefig(buf, format='png')
+    buf.seek(0)
+    st.session_state["fig"] = buf
     return fig
 
 
 st.text("data_points: \n データの数")
-data_points = st.slider("data_points", 1000, 660000, 10000)
+data_points = st.slider("data_points", 1000, 66000, 10000)
 st.text("n_neighbors: \n 値が小さいほど局所的なデータが保持されます")
 n_neighbors = st.slider("n_neighbors", 2, 100, 15)
 st.text("min_dist: \n 埋め込み空間における点間の最小距離を定義")
